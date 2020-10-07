e953e38ee28c479e7e3fb8afd8e44d56c686018f
Tejeswar <mbtejeshwarreddy@gmail.com>
2020-10-07 22:02:04 +0530

    store selected vehicle in Redux

--- src/store/reducers/Reducer.js
+++ src/store/reducers/Reducer.js
@@ -21,10 +21,12 @@ export default (state=initialState,action)=>{
             selectedPlanets[action.index]=  action.planetName;
             return {...state,selectedPlanets:selectedPlanets}
 
-        case 'STORE_SELECTED_VEHICLE':
+        case 'STORE_SELECTED_VEHCILE':
             let selectedVehicles = [...state.selectedVehicles]
             selectedVehicles[action.index]=  action.vehicleName;
-            // let vehicles = {...state.vehicles}
+            // let vehicles = [...state.vehicles];
+            // vehicles[action.index].total_no = state.vehicles[action.index].total_no - 1;
+            // console.log(state.vehicles);
             return {...state,selectedVehicles:selectedVehicles}
 
         default:
