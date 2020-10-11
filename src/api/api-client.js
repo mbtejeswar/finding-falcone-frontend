8009a7273231f6cafc6fbe5bd21cc99be520b9da
Tejeswar <mbtejeshwarreddy@gmail.com>
2020-10-11 22:01:27 +0530

    add token and findFalcone API

--- src/api/api-client.js
+++ src/api/api-client.js
@@ -1,25 +1,18 @@
 import axios from 'axios';
 
-const endpoint = process.env.REACT_APP_SERVICE_URI? process.env.REACT_APP_SERVICE_URI : 'https://findfalcone.herokuapp.com/'
-
-// export const {
-//   getAllProducts () {
-//       return axios.get(endpoint + 'products').then(response => {
-//         log.debug(`api client fetched ${response.data.length} items`)
-//         return response.data
-//       }).catch(err => {
-//         log.error(err.message)
-//         throw err
-//       })
-//     } 
-//   }
-
-
+const endpoint = process.env.REACT_APP_SERVICE_URI? process.env.REACT_APP_SERVICE_URI : 'https://findfalcone.herokuapp.com'
 
   export const fetchPlanets = () =>{
-    return axios.get(`${endpoint}planets`);
+    return axios.get(`${endpoint}/planets`);
   };
   export const fetchVehicles = () =>{
-    return axios.get('https://findfalcone.herokuapp.com/vehicles');
+    return axios.get(`${endpoint}/vehicles`);
   };
+
+  export const fetchToken = ()=>{
+    return axios.get(`${endpoint}/token`);
+  }
+  export const findFalcone = (token,selectedPlanets,selectedVehicles)=>{
+    return axios.post(`${endpoint}/findFalcone`);
+  }
 
